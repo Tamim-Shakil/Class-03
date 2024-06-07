23d3c87c9e6f93aff500a76e9e497feb4047f5ac
Tamim-Shakil <tamim720sk@gmail.com>
2024-06-07 15:55:32 +0600

    complete project

--- Class 16/object.js
+++ Class 16/object.js
@@ -1,9 +1,32 @@
 let person = {
   name: "Shakil",
-  age: 20,
+  age: 10,
   profession: "web developer",
-  isMarried: false,
+  isMarried: true,
+  showMessage: function () {
+    console.log(this.name, " is a", this.profession);
+  },
+
+  showIfMarried() {
+    if (this.isMarried) {
+      console.log("Shakil is married");
+    } else {
+      console.log("Shakil is unmarried");
+    }
+  },
+
+  checkIfAddult: function () {
+    if (this.age > 18) {
+      console.log(this.name, "is an adult");
+    } else {
+      console.log(this.name, "still a child!");
+    }
+  },
 };
+
+person.showMessage();
+person.showIfMarried();
+person.checkIfAddult();
 
 let car = {
   brand: "BMW",
@@ -18,17 +41,17 @@ let car2 = {
   color: "Red",
 };
 
-// dot notation
-console.log(person.name);
-console.log(person.age);
-console.log(person.profession);
+// // dot notation
+// console.log(person.name);
+// console.log(person.age);
+// console.log(person.profession);
 
-console.log(car.brand);
-console.log(car.model);
-console.log(car.color);
-console.log(car.weight);
+// console.log(car.brand);
+// console.log(car.model);
+// console.log(car.color);
+// console.log(car.weight);
 
-console.log(person["age"]); // bracket notation
+// console.log(person["age"]); // bracket notation
 
 // console.table(car2);
-console.log(car2);
+// console.log(car2);
